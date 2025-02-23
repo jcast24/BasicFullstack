8da2a388a4a8116142bd0912c09edd1718044b54
Jesus Castro <castrojesus39@gmail.com>
2025-02-22 23:12:32 -0800

    insert file for items into db

---
+++ insert.js
@@ -0,0 +1,17 @@
+import sqlite3 from "sqlite3";
+import { execute } from "./sql.js";
+
+const main = async () => {
+    const db = new sqlite3.Database("test.db");
+    const sql = `INSERT INTO products(name, price) VALUES (?, ?)`;
+
+    try {
+        await execute(db, sql, ["iPhone", 899.99]);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        db.close();
+    }
+};
+
+main();
