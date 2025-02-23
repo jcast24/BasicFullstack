b1733f2a8289ecf2689618297be214a4bd59c0de
Jesus Castro <castrojesus39@gmail.com>
2025-02-22 22:37:30 -0800

    created sql table

---
+++ index.js
@@ -0,0 +1,22 @@
+import sqlite3 from "sqlite3";
+import { execute } from "./sql.js";
+
+const main = async () => {
+  const db = new sqlite3.Database("test.db");
+  try {
+    await execute(
+      db,
+      `CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        price DECIMAL(10, 2) NOT NULL
+        )`,
+    );
+  } catch (error) {
+    console.log(error);
+  } finally {
+    db.close();
+  }
+};
+
+main();
