939d290b50f1bddfe6a1715135624aca1b121974
Jesus Castro <castrojesus39@gmail.com>
2025-02-22 23:13:59 -0800

    updated wrapper function

--- sql.js
+++ sql.js
@@ -1,4 +1,13 @@
-export const execute = async (db, sql) => {
+export const execute = async (db, sql, params = []) => {
+  if (params && params.length > 0) {
+    return new Promise((resolve, reject) => {
+      db.run(sql, params, (err) => {
+        if (err) reject(err);
+        resolve();
+      });
+    });
+  }
+
   return new Promise((resolve, reject) => {
     db.exec(sql, (err) => {
       if (err) reject(err);
