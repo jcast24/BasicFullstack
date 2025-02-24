946f0f3b934aea587e13e79fd86317d7ade8cb02
Jesus Castro <castrojesus39@gmail.com>
2025-02-23 22:19:02 -0800

    fetchAll method created using db.all

--- sql.js
+++ sql.js
@@ -1,3 +1,13 @@
+/*
+ * Wrapper function
+ *
+ * Calls the run method and returns a promise.
+ *
+ * The execute() function calls the run() method if the params parameter has items
+ * or the exec() method if it does not.
+ *
+ * */
+
 export const execute = async (db, sql, params = []) => {
   if (params && params.length > 0) {
     return new Promise((resolve, reject) => {
@@ -12,6 +22,15 @@ export const execute = async (db, sql, params = []) => {
     db.exec(sql, (err) => {
       if (err) reject(err);
       resolve();
+    });
+  });
+};
+
+export const fetchAll = async (db, sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) reject(err);
+      resolve(rows);
     });
   });
 };
